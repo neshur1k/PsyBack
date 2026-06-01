bc9a00a9c832cecd196b207d8185f03e587aa994
Alexander <alexander.angatkin@gmail.com>
2026-06-01 08:45:24 +0300

    task 6

--- src/main/kotlin/data/repository/NobelRepositoryImpl.kt
+++ src/main/kotlin/data/repository/NobelRepositoryImpl.kt
@@ -1,8 +1,13 @@
 package com.example.data.repository
 
+import com.example.data.db.table.LaureatesTable
+import com.example.data.db.table.PrizesTable
 import com.example.data.model.Laureate
 import com.example.data.model.Prize
 import com.example.domain.repository.NobelRepository
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
 
 class NobelRepositoryImpl : NobelRepository {
 
@@ -35,7 +40,31 @@ class NobelRepositoryImpl : NobelRepository {
         )
     )
 
-    override fun getPrizes() = prizes
+    override fun getPrizes(): List<Prize> = transaction {
+
+        val prizes = PrizesTable.selectAll().map { row ->
+            val prizeId = row[PrizesTable.id]
+
+            val laureates = LaureatesTable
+                .select { LaureatesTable.prizeId eq prizeId }
+                .map {
+                    Laureate(
+                        id = it[LaureatesTable.id].toString(),
+                        fullName = it[LaureatesTable.fullName],
+                        motivation = it[LaureatesTable.motivation],
+                        country = it[LaureatesTable.portraitUrl] ?: ""
+                    )
+                }
+
+            Prize(
+                year = row[PrizesTable.awardYear],
+                category = row[PrizesTable.category],
+                laureates = laureates
+            )
+        }
+
+        prizes
+    }
 
     override fun getPrize(
         year: String,
